c16da42964fff37d481192a328d51f42214bbb42
Julio Rodriguez <jerodriguezb@gmail.com>
2021-06-28 10:24:32 -0300

    heroku 3

--- server/config/config.js
+++ server/config/config.js
@@ -7,16 +7,20 @@ process.env.SEED = process.env.SEED || "esta_es_la_firma";
 //La fecha de expiracion del token
 process.env.EXPIRACION = "48h";
 
+process.env.NODE_ENV = process.env.NODE_ENV || "dev";
+
 let urlDB;
 
-//if (process.env.MODE_ENV==="dev") {
+if (process.env.MODE_ENV === "dev") {
+  urlDB = "mongodb://localhost:27017/academy";
+} else {
+  urlDB = process.env.MONGO_URI;
+}
 
-urlDB =
-  //"mongodb://cc40df585d057fdd63c20ed9414b027f:kaka8596@12a.mongo.evennode.com:27018/cc40df585d057fdd63c20ed9414b027f";
+process.env.URLDB = urlDB;
 
-  //"mongodb://cc40df585d057fdd63c20ed9414b027f:kaka8596@12a.mongo.evennode.com:27018,12b.mongo.evennode.com:27018/52f8f3c80ee6733ef1b477e6216e5877?authSource=cc40df585d057fdd63c20ed9414b027f&replicaSet=us-12%22%20-u%2052f8f3c80ee6733ef1b477e6216e5877&readPreference=primary&appname=MongoDB%20Compass&ssl=false";
+//"mongodb://cc40df585d057fdd63c20ed9414b027f:kaka8596@12a.mongo.evennode.com:27018/cc40df585d057fdd63c20ed9414b027f";
 
-  "mongodb+srv://user:kakaroto@cluster0.47bwt.mongodb.net/academy";
-//}
+//"mongodb://cc40df585d057fdd63c20ed9414b027f:kaka8596@12a.mongo.evennode.com:27018,12b.mongo.evennode.com:27018/52f8f3c80ee6733ef1b477e6216e5877?authSource=cc40df585d057fdd63c20ed9414b027f&replicaSet=us-12%22%20-u%2052f8f3c80ee6733ef1b477e6216e5877&readPreference=primary&appname=MongoDB%20Compass&ssl=false";
 
-process.env.urlDB = urlDB;
+//"mongodb+srv://user:kakaroto@cluster0.47bwt.mongodb.net/academy";
